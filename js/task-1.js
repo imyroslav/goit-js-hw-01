c5a2006f2a40dbbaf21596f65564d725fe2c5a42
imyroslav <imyroslav@ukr.net>
2023-12-25 02:06:11 +0100

    task-1

---
+++ js/task-1.js
@@ -0,0 +1,10 @@
+function makeTransaction(quantity, pricePerDroid) {
+    let totalPrice = quantity*pricePerDroid;
+    console.log(`You orderded ${quantity} droids worth ${totalPrice} credits! `)
+}
+
+
+
+
+
+

f2098582a0f703ff3e25fa74f99d1bddd82d1400
imyroslav <imyroslav@ukr.net>
2024-01-19 06:30:59 +0700

    removed-alert

--- js/task-1.js
+++ js/task-1.js
@@ -1,10 +1,12 @@
 function makeTransaction(quantity, pricePerDroid) {
     let totalPrice = quantity*pricePerDroid;
-    alert(`You orderded ${quantity} droids worth ${totalPrice} credits! `)
-};
+    console.log(`"You orderded ${quantity} droids worth ${totalPrice} credits!"`);
+}
 
 
-
+console.log(makeTransaction(5, 3000)); 
+console.log(makeTransaction(3, 1000)); 
+console.log(makeTransaction(10, 500));
 
 
 
--- js/task-3.js
+++ js/task-3.js
@@ -1,4 +1,12 @@
-function getElementhWidth(content, padding, border) {
-    alert(`Element with is ${1*content + 2*padding + 2*border} px. `)
+function getElementWidth(content, padding, border) {
+    let contentN = Number.parseFloat(content);
+    let paddingN = Number.parseFloat(padding);
+    let borderN = Number.parseFloat(border);
+    return contentN + 2*paddingN + 2*borderN;
 }
+
+console.log(getElementWidth("50px", "8px", "4px")); 
+console.log(getElementWidth("60px", "12px", "8.5px"));
+console.log(getElementWidth("200px", "0px", "0px")); 
+
 
